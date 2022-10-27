aba4796817edea6653794a73d489d7bcc5bc6ee1
nedosika <nedosika@gmail.com>
2022-10-27 21:57:27 +0300

    add settings for heroku

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import {graphqlHTTP} from "express-graphql";
 import schema from "./graphql/schema.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
